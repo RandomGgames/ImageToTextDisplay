f7e6e8f14c9162cf2e0a1cc08fb197eff95b8489
RandomGgames <gianopp@gmail.com>
2023-11-12 21:50:14 -0500

    Added version number

--- Convert.py
+++ Convert.py
@@ -4,6 +4,9 @@ import sys
 import yaml
 from PIL import Image
 
+__version_info__ = (0, 1, 0)
+__version__ = '.'.join(str(x) for x in __version_info__)
+
 def loadImage(path: str) -> Image:
     logging.debug(f'Reading the image file at "{path}".')
     try:
